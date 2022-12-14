227bee97be2612ef1d4111540ded57dc56e408b8
Abiodun <aabiodunawoyemi@gmail.com>
2022-12-14 22:45:28 +0100

    abiodun: crowd fund flow

--- src/config/index.js
+++ src/config/index.js
@@ -2,13 +2,14 @@ import DAO from "../utils/abi/DAO.json"
 import SUSDC from "../utils/abi/SUSDC.json"
 import USDCMinting from "../utils/abi/usdcMinting.json"
 import StableBank from "../utils/abi/StableBank.json"
+import CrowdFundFactory from "../utils/abi/crowdFundFactory.json"
 
-export const DAO_ADDRESS = "0x0c7C33686486A2e3a1f8E4F0c3F7584d0bf00dEC"
-export const DAO_TOKEN_ADDRESS = "0x799A21ee3C1264e31382E50B95191f6A8A82868d"
-export const sUSDC_TOKEN_ADDRESS = "0xC4D5D81271bc6a4585F248a5593f1B84030be584"
-export const CROWDFUND_FACTORY_ADDRESS = "0x0c7C33686486A2e3a1f8E4F0c3F7584d0bf00dEC"
+export const DAO_ADDRESS = "0x6Af366e15196D13997287A5F7760c7cE5C18a291"
+export const DAO_TOKEN_ADDRESS = "0x2144683943E349562830B5bB2D559650B46F1f11"
+export const sUSDC_TOKEN_ADDRESS = "0x5468b0FDdC8DB2FDCb665EFf28eb35Bbf0AF342d"
+export const CROWDFUND_FACTORY_ADDRESS = "0xA5641e07F05cE12D2a7F35Cc549421dC1c141eF2"
 export const MINTING_CONTRACT_ADDRESS = "0x238908C108414b5db5A74655e487e1F0a16673Ff"
-export const DAO_FUNDY_NFT_ADDRESS = "0x4d5D3ef4677De7638f109681C06893591749F7EE"
+export const DAO_FUNDY_NFT_ADDRESS = "0xc5167Df8A995b4A89D27c430ECB393106aF7CFa9"
 
 
 export const DAO_CONTRACT = {
@@ -31,4 +32,8 @@ export const DAO_TOKEN_CONTRACT = {
     abi: StableBank.abi,
 }
 
+export const CROWDFUND_FACTORY_CONTRACT = {
+    address: CROWDFUND_FACTORY_ADDRESS,
+    abi: CrowdFundFactory.abi,
+}
 
